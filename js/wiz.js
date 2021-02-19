76eef1d11cbdc236a4dc3ae65966030cd27bff27
Ian Holmes <ihholmes@gmail.com>
2021-02-18 23:49:33 -0800

    Added a few cards

--- js/wiz.js
+++ js/wiz.js
@@ -1,2 +1,15 @@
 const cards =
-      [{ html: 'Game Over', priority: -1 }]
+      [{ limit: 1,
+         cards: [
+           "An opportunity presents itself to become the school drug dealer.",
+           "Bright red graffiti across the bike sheds proclaims that elves suck. Will you help solve this magical crime?",
+           "Would you like to learn how to ride a were-horse?",
+           "You arrive early for registration to find your form-master tied to a silver chair and sobbing uncontrollably. Will you free him?",
+           "Your best friend suggests that you start a school newspaper for wizards.",
+           "You notice that your peers like to place bets on astrological conjunctions. It occurs to you to start a gambling operation.",
+           "In order to impress your crush, you conjure forth the evil from within you as a nameless shadow.",
+           "A classmate has invented a new cryptocurrency powered by actual dwarf miners. Are you in?",
+           "It's about this time that many entrepreneurial magical students form their own startup.",
+           "You're surprised by a spot quiz on ethical use of doppelgangers. Will you cheat?",
+         ] },
+       { html: 'Game Over', priority: -1 }]
